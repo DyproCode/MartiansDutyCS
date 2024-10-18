26c6ab8080c83bb8566f80fb0ccaf097b6c959f6
keanukk <keanufahr@gmail.com>
2024-10-18 18:05:20 +0200

    adding new tileset and dash

--- scripts/GUIComponents/start_screen.cs
+++ scripts/GUIComponents/start_screen.cs
@@ -3,13 +3,13 @@ using System;
 
 public partial class start_screen : Control
 {
-    private void _on_start_btn_pressed()
-    {
-        GetTree().ChangeSceneToFile("res://scenes/Screens/main_game.tscn");
-    }
+	private void _on_start_btn_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/Screens/main_game.tscn");
+	}
 
-    private void _on_quit_btn_pressed()
-    {
-        GetTree().Quit();
-    }
+	private void _on_quit_btn_pressed()
+	{
+		GetTree().Quit();
+	}
 }
