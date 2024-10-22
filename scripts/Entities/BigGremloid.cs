5d56212947aa02132e5b6aed9adf57397e5a6424
DyproCode <dylant.proctor@gmail.com>
2024-10-22 15:52:28 -0400

    AddedBigGremloid

---
+++ scripts/Entities/BigGremloid.cs
@@ -0,0 +1,71 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MartiansDutyCS.scripts.Effects;
+using MartiansDutyCS.scripts.Entities;
+using MartiansDutyCS.scripts.Systems;
+
+public partial class BigGremloid : BaseEnemy
+{
+    //Health
+    public static int MaxHealth = 15;
+    public static float Speed = 150;
+    public static int MoneyDrop = 5;
+    
+    public override void _Ready()
+    {
+        this.Damage = 2;
+        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        _healthComponent = GetNode<HealthComponent>("HealthComponent");
+        _healthComponent.Initialize(MaxHealth);
+        
+        Triggers.Add(
+            new Trigger(TriggerType.Die,
+                new List<IEffect>
+                {
+                    new EffectIncreasePlayerMoney(MoneyDrop)
+                }
+            ));
+    }
+
+    public override void _Process(double delta)
+    {
+        var player = GetTree().GetNodesInGroup("Player").First() as PlayerScene;
+        _sprite.LookAt(player.GlobalPosition);
+        if (State == "walking" && GlobalPosition.DistanceTo(player.GlobalPosition) > 10)
+        { 
+            Velocity = GlobalPosition.DirectionTo(player.GlobalPosition) * Speed;
+        }
+        else
+        {
+            Velocity = GlobalPosition.DirectionTo(player.GlobalPosition) * -1;
+        }
+        
+        SetAnimation();
+        MoveAndSlide();
+    }
+    
+    private void SetAnimation()
+    {
+        if (State == "walking")
+        {
+            if (_healthComponent.CurrentHealth <= _healthComponent.MaxHealth * 0.5)
+            {
+                _sprite.Animation = "walking_dying";
+            }
+            else if (_healthComponent.CurrentHealth < _healthComponent.MaxHealth)
+            {
+                _sprite.Animation = "walking_hurt";
+            }
+            else
+            {
+                _sprite.Animation = "walking_full";
+            }
+        }
+        else if (State == "attacking")
+        {
+            _sprite.Animation = "attacking";
+        }
+    }
+}
